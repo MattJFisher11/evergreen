72cd458d5d9ed17bbffea045e9a230850a5a2875
Matthew Fisher <matthewjamesfisher@hotmail.com>
2025-06-03 13:02:22 +0100

    feat added logic for the output to calculate costs and display data

--- src/index.js
+++ src/index.js
@@ -9,25 +9,40 @@ const API_KEY = 'f661f74e-20a7-4e9f-acfc-041cfb846505';
 const WEATHER_API = 'https://063qqrtqth.execute-api.eu-west-2.amazonaws.com/v1/weather?location=';
 
 async function processHouse(house) {
+    //key properties from the house object for easier access
     const { submissionId, floorArea, heatingFactor, insulationFactor, designRegion} = house;
+    //calculate the heatloss
     const heatLoss = floorArea * heatingFactor * insulationFactor;
     try {
+        // Fetch weather data
         const res = await fetch(`${WEATHER_API}${encodeURIComponent(designRegion)}`,
         {headers: {'x-api-key': API_KEY}})
 
         if (res.status === 200) {
             const data = await res.json();
+            //gets the degreeDays fromm the weather api
+            const degreeDays = data.location.degreeDays;
+            //calculates the power heat loss using total heat loss calculated and the returned heating degree days
+            const powerHeatLoss = heatLoss / degreeDays;
+            //finds the first heat pump from the list that meats the condition.
+            const pump = heatPumpData.find(p => p.outputCapacity >= powerHeatLoss);
+            // combines all elements into a single result and add the cost up starting from 0
+            const subtotal = pump.costs.reduce((sum, c) => sum + c.cost, 0);
+            // Create an array of formatted cost item strings "label, amount") for display
+            const costs = pump.costs.map(pumpCost => `${pumpCost.label}, £${pumpCost.cost}`);
+            //calculates the total amount with vat
+            const totalWithVAT = (0.05 * subtotal) + subtotal;
             return `
             --------------------------------------
             ${submissionId}
             --------------------------------------
-            Estimated Heat Loss = ${heatLoss.toFixed(2)}
-            Design Region = 
-            Power Heat Loss = 
-            Recommended Heat Pump = 
+            Estimated Heat Loss = ${heatLoss.toFixed(2) + "kWh"}
+            Design Region = ${designRegion}
+            Power Heat Loss = ${powerHeatLoss.toFixed(2)+ "kW"}
+            Recommended Heat Pump = ${pump.label}
             Cost Breakdown
-            <label>, <cost>
-            Total Cost, including VAT = £;
+            ${costs}
+            Total Cost, including VAT = £${totalWithVAT};
             `
         }else if (res.status === 404){
             return `
